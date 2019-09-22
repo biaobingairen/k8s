e530c6d487efcff7f68c34da4c2fa4e348194b6b
biaobingairen <40482449+biaobingairen@users.noreply.github.com>
2019-09-22 23:54:28 +0800

    Add files via upload

---
+++ Master.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#只在master执行
+export MASTER_IP=10.156.122.124
+export APISERVER_NAME=apiserver-k8s
+export POD_SUBNET=10.100.0.1/20
+echo "${MASTER_IP}    ${APISERVER_NAME}" >> /etc/hosts
+rm -f ./kubeadm-config.yaml
+cat <<EOF > ./kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+kubernetesVersion: v1.16.0
+imageRepository: registry.cn-hangzhou.aliyuncs.com/google_containers
+controlPlaneEndpoint: "${APISERVER_NAME}:6443"
+networking:
+  serviceSubnet: "10.96.0.0/12"
+  podSubnet: "${POD_SUBNET}"
+  dnsDomain: "cluster.local"
+EOF
+
+# kubeadm init
+# 根据您服务器网速的情况，您需要等候 3 - 10 分钟
+kubeadm init --config=kubeadm-config.yaml --upload-certs
+
+# 配置 kubectl
+rm -rf /root/.kube/
+mkdir /root/.kube/
+cp -i /etc/kubernetes/admin.conf /root/.kube/config
+
+# 安装 calico 网络插件
+# 参考文档 https://docs.projectcalico.org/v3.8/getting-started/kubernetes/
+rm -f calico.yaml
+wget https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+sed -i "s#192\.168\.0\.0/16#${POD_SUBNET}#" calico.yaml
+kubectl apply -f calico.yaml
